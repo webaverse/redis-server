b2b97af6ccd90c5325d427fb1d0345eb8c164c7f
Avaer Kazmer <a@modules.io>
2021-04-14 17:27:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ fs.writeFileSync('./redis.conf', redisConfTxt);
 const cp = child_process.spawn('./redis-server', [
   './redis.conf',
 ]);
+cp.on('error', err => {
+  console.warn(err);
+  process.exit(1);
+});
 cp.stdout.setEncoding('utf8');
 cp.stdout.on('data', s => {
   console.log('got data', s);
