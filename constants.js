56fb55d5707c949aa441fa8161ebdfed57c9ad11
Avaer Kazmer <a@modules.io>
2021-04-14 16:44:29 -0400

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,18 @@
+const ids = {
+  lastCachedBlockAccount: 'lastCachedBlock',
+  lastCachedBlockNft: -1,
+};
+const tableNames = {
+  mainnetAccount: 'mainnet-cache-account',
+  mainnetNft: 'mainnet-cache-nft',
+  mainnetsidechainAccount: 'sidechain-cache-account',
+  mainnetsidechainNft: 'sidechain-cache-nft',
+  testnetAccount: 'testnet-cache-account',
+  testnetNft: 'testnet-cache-nft',
+  testnetsidechainAccount: 'testnetsidechain-cache-account',
+  testnetsidechainNft: 'testnetsidechain-cache-nft',
+  polygonAccount: 'polygon-cache-account',
+  polygonNft: 'polygon-cache-nft',
+  testnetpolygonAccount: 'testnetpolygon-cache-account',
+  testnetpolygonNft: 'testnetpolygon-cache-nft',
+};
