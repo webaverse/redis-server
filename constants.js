534dea2acde92d58e5d8ab4a29703dab6a792d19
Avaer Kazmer <a@modules.io>
2021-04-14 16:46:38 -0400

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,18 @@
+const accountKeys = [
+  'name',
+  'avatarId',
+  'avatarName',
+  'avatarExt',
+  'avatarPreview',
+  'loadout',
+  'homeSpaceId',
+  'homeSpaceName',
+  'homeSpaceExt',
+  'homeSpacePreview',
+  'ftu',
+  // 'mainnetAddress',
+  'addressProofs',
+];
 const ids = {
   lastCachedBlockAccount: 'lastCachedBlock',
   lastCachedBlockNft: -1,
@@ -15,4 +30,17 @@ const tableNames = {
   polygonNft: 'polygon-cache-nft',
   testnetpolygonAccount: 'testnetpolygon-cache-account',
   testnetpolygonNft: 'testnetpolygon-cache-nft',
+};
+const polygonVigilKey = `1bdde9289621d9d420488a9804254f4a958e128b`;
+const ethereumHost = 'ethereum.exokit.org';
+const storageHost = 'https://ipfs.exokit.org';
+const mainnetSignatureMessage = `Connecting mainnet address.`;
+module.exports = {
+  accountKeys,
+  ids,
+  tableNames,
+  polygonVigilKey,
+  ethereumHost,
+  storageHost,
+  mainnetSignatureMessage,
 };
