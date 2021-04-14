c849a109f2264639362fe8a0223efec46a7e2514
Avaer Kazmer <a@modules.io>
2021-04-14 16:57:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const cache = require('./cache.js');
+const fs = require('fs');
+const child_process = require('child_process');
+const {initCaches} = require('./cache.js');
+const {redisKey} = require('./config.json');
+
+let redisConfTxt = fs.readFileSync('./redis.conf.template', 'utf8');
+redisConfTxt = redisConfTxt.replace(/# requirepass foobared/, `requirepass ${redisKey}`);
+fs.writeFileSync('./redis.conf', redisConfTxt);
+
+const cp = child_process.spawn('./redis-server', [
+  'redis.conf',
+]);
+cp.stdout.setEncoding('utf8');
+cp.stdout.on('data', s => {
+  console.log('got data', s);
+});
+cp.on('exit', code => {
+  console.warn(`redis exited with code ${code}`);
+  process.exit(code);
+});
