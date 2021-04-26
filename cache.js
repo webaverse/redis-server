3661a1940341d0b248d68779fed0e2813a1f981d
Avaer Kazmer <a@modules.io>
2021-04-25 23:59:26 -0400

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,6 +1,6 @@
 const {getRedisItem, putRedisItem} = require('./redis.js');
 const {getChainNft, getChainAccount, getAllWithdrawsDeposits} = require('./tokens.js');
-const {ids, redisPrefixes} = require('./constants.js');
+const {propertiesKeys, ids, redisPrefixes} = require('./constants.js');
 const {getBlockchain, getPastEvents, makeWeb3WebsocketContract} = require('./blockchain.js');
 const {connect, getRedisAllItems} = require('./redis.js');
 
