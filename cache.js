2c6bfc7c70e41985215ee9d95e33623108352f35
Avaer Kazmer <a@modules.io>
2021-09-15 15:53:59 -0400

    Comment out polygon endpoint

--- cache.js
+++ cache.js
@@ -171,7 +171,7 @@ async function initCaches() {
   await Promise.all([
     'mainnet',
     'mainnetsidechain',
-    'polygon',
+    // 'polygon',
   ].map(chainName => {
     return Promise.all([
       _logCache(chainName + ' NFT', initNftCache({chainName})),
@@ -191,8 +191,8 @@ async function processEventNft({event, chainName}) {
         mainnetWithdrewEntries,
         sidechainDepositedEntries,
         sidechainWithdrewEntries,
-        polygonDepositedEntries,
-        polygonWithdrewEntries,
+        // polygonDepositedEntries,
+        // polygonWithdrewEntries,
       } = await getAllWithdrawsDeposits('NFT')(chainName);
 
       const token = await getChainNft('NFT')(chainName)(
@@ -202,8 +202,8 @@ async function processEventNft({event, chainName}) {
         mainnetWithdrewEntries,
         sidechainDepositedEntries,
         sidechainWithdrewEntries,
-        polygonDepositedEntries,
-        polygonWithdrewEntries,
+        // polygonDepositedEntries,
+        // polygonWithdrewEntries,
       );
 
       if (token.owner.address !== '0x0000000000000000000000000000000000000000') {
@@ -298,8 +298,8 @@ async function processEventsNft({events, currentBlockNumber, chainName}) {
     mainnetWithdrewEntries,
     sidechainDepositedEntries,
     sidechainWithdrewEntries,
-    polygonDepositedEntries,
-    polygonWithdrewEntries,
+    // polygonDepositedEntries,
+    // polygonWithdrewEntries,
   } = await getAllWithdrawsDeposits('NFT')(chainName);
   for (const tokenId of tokenIds) {
     const token = await getChainNft('NFT')(chainName)(
@@ -309,8 +309,8 @@ async function processEventsNft({events, currentBlockNumber, chainName}) {
       mainnetWithdrewEntries,
       sidechainDepositedEntries,
       sidechainWithdrewEntries,
-      polygonDepositedEntries,
-      polygonWithdrewEntries,
+      // polygonDepositedEntries,
+      // polygonWithdrewEntries,
     );
 
     if (token.owner.address !== '0x0000000000000000000000000000000000000000') {
